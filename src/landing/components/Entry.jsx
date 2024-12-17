47bd1d799e212ead8bc174548ad4f0b67bdc554e
Amirl_kl34@itstep.edu.az <Amirl_kl34@itstep.edu.az>
2024-12-18 00:47:21 +0400

    test2

--- src/landing/components/Entry.jsx
+++ src/landing/components/Entry.jsx
@@ -2,18 +2,26 @@ import React from "react";
 import Navbar from "./Navbar";
 const Entry = () => {
   return (
+
     <section
-      className="relative h-screen w-full bg-cover bg-center"
+      className="relative h-screen w-full bg-cover bg-center "
       style={{
         backgroundImage:
           "url('https://assets.nflxext.com/ffe/siteui/vlv3/729ce5c2-d831-436a-8c9d-f38fea0b99b3/web/AZ-en-20241209-TRIFECTA-perspective_a09d6162-5992-4ec3-a2b8-9081ca80c5b0_large.jpg')",
       }}
-    >
+    >  
       {/* Qaranlıq Overlay */}
       <Navbar />
       <div className="absolute inset-0 bg-black opacity-60"></div>
 
       {/* Məzmun */}
+
+
+
+
+
+
+ {/* ///////// qovs*/}
       <div className="relative z-10 flex flex-col justify-center items-center h-full text-white text-center"></div>
       <div
         class="relative -mt-[230px] h-[100px] bg-gradient-to-b from-black via-transparent to-transparent 
@@ -22,7 +30,7 @@ const Entry = () => {
       className=" h-[50px] w-full bg-gradient-to-b rounded-tl-[50%_90%] rounded-tr-[50%_90%]
   from-blue-950 from-10% to-black to-90%"
     ></div></div>
-      {/* ///////// */}
+     
      
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}", // Düzgün yol qeyd edilib
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        netflix: ['"Netflix Sans"', '"Helvetica Neue"', '"Segoe UI"', 'Roboto', 'Ubuntu', 'sans-serif'],
+      },
+      colors: {
+        netflixRed: '#E50914', // Netflix qırmızı rəngi
+      },},
   },
   plugins: [],
 };
